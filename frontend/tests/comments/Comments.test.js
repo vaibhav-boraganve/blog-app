44b271cf1864e8f143506628920770324c639b95
vaibhav-boraganve <vaibhavboraganve109@gmail.com>
2022-11-20 19:13:50 +0530

    unwanted files removed

--- frontend/tests/comments/Comments.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Comments from "../../src/components/comments/Comments";
-import "@testing-library/jest-dom/extend-expect";
-
-test("renders comments component", async () => {
-  const { getByText } = render(<Comments />);
-  expect(getByText("No comments yet")).toBeInTheDocument();
-});
